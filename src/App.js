e656acb8e745c55839cbcefad799c9c4731c4a9f
Akoth Cathy <catherinececilia22@gmail.com>
2022-01-28 05:43:20 -0800

    More Added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import ContactUs from './components/pages/ContactUs';
-import SignUp from './components/pages/SignUp';
-import Marketing from './components/pages/Marketing';
-import Consulting from './components/pages/Consulting';
+import React from "react";
+import HomePage from "./pages/homePage";
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/services' component={Services} />
-        <Route path='/products' component={Products} />
-        <Route path='/contact-us' component={ContactUs} />
-        <Route path='/sign-up' component={SignUp} />
-        <Route path='/marketing' component={Marketing} />
-        <Route path='/consulting' component={Consulting} />
-      </Switch>
-    </Router>
-  );
-}
+const App = () => {
+  return <HomePage />;
+};
 
 export default App;
